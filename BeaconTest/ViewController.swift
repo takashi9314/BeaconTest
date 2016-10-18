8ac2d74c32c9b492b5a0c718261681dad1f31a87
松本隆 <takashi.matsumoto@matsumoto-no-MacBook-Pro.local>
2016-10-18 18:03:38 +0900

    Initial Commit

---
+++ BeaconTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BeaconTest
+//
+//  Created by 松本 隆 on 2016/10/18.
+//  Copyright © 2016年 Favy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
